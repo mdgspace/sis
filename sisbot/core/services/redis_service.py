5fa5bdda85c94e1fdfbe90fde6e89360741c02ae
Amey <ameykhandalkar5921@gmail.com>
2023-10-17 15:21:29 +0530

    refactor: adding redis class

--- sisbot/core/services/redis_service.py
+++ sisbot/core/services/redis_service.py
@@ -1,19 +1,43 @@
 import redis
 import os
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 
+class RedSis:
+  r=0
+  
+  def redisInit(self):
+    try:
+      self.r = redis.Redis(
+        host=os.environ.get("REDIS_HOST"),
+        port=os.environ.get("REDIS_PORT"),
+        password=os.environ.get("REDIS_PASSWORD")
+      )
+      #could be better connections depending on the bot using the store
+      print("[REDIS] Connection successful")
+    except:
+      print("[REDIS] Connection Failed")
+    
+  def setValue(self, key:str, value):
+    try:
+      if(isinstance(value,dict)):
+        value_json = json.dumps(value)
+        self.r.set(key, value_json)
+        return
 
-r = redis.Redis(
-  host=os.environ.get("REDIS_HOST"),
-  port=os.environ.get("REDIS_PORT"),
-  password=os.environ.get("REDIS_PASSWORD")
-)
+      self.r.set(key, value)
+    except:
+      print(f"[REDIS] error in setting the value [{key}]")
+  
+  def getValue(self, key):
+    return self.r.get(key).decode("utf-8")
+    
 
-# Redis commands
-r.set('hello', 'world')
-print(r.get('hello'))
+# lol = RedSis()
+# lol.redisInit()
+  
 
 if __name__ == "__main__":
     pass
